892a24914c8cfd29adbb686832114458fdfac3b3
Gnurro <knawurzelkopp@hotmail.de>
2024-08-12 15:21:31 +0200

    Add custom model registry to run with LLaMA2-13B-Tiefighter and Mixtral-8x7B-Instruct-v0.1; Add shell scripts to run model on GPU cluster

---
+++ adventuregame_runs_large.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Usage: ./pipeline_huggingfaces.sh
+# Preparation: ./setup_hf.sh
+export HUGGINGFACE_HUB_CACHE=/data/jordan/huggingface_cache
+export CUDA_VISIBLE_DEVICES=0,1,2
+# activate HF venv:
+source venv_hf/bin/activate
+source prepare_path.sh
+# run pipeline:
+echo
+echo "==================================================="
+echo "PIPELINE: Starting"
+echo "==================================================="
+echo
+game_runs=(
+  # Single-player: adventuregame
+  "adventuregame Mistral-Large-Instruct-2407"
+)
+total_runs=${#game_runs[@]}
+echo "Number of benchmark runs: $total_runs"
+current_runs=1
+for run_args in "${game_runs[@]}"; do
+  echo "Run $current_runs of $total_runs: $run_args"
+  bash -c "./run.sh ${run_args}"
+  ((current_runs++))
+done
+echo "==================================================="
+echo "PIPELINE: Finished"
+echo "==================================================="
