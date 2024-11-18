fe541b5e127138df0c3e772c62b799afb1d7fc9b
Gnurro <knawurzelkopp@hotmail.de>
2024-11-18 11:54:35 +0100

    Add PDDL parsing prototype with initial grammar and PDDL test files

---
+++ games/adventuregame/pddl_experiments.py
@@ -0,0 +1,203 @@
+import lark
+from lark import Lark, Transformer
+
+
+with open("pddl_actions.lark") as grammar_file:
+    action_grammar = grammar_file.read()
+
+# print(action_grammar)
+
+parser = Lark(action_grammar, start="action")
+
+with open("test_pddl_actions.txt") as action_file:
+    action_raw = action_file.read()
+
+
+parsed_action = parser.parse(action_raw)
+
+# print(parsed_action)
+
+
+class PDDLActionTransformer(Transformer):
+    """PDDL action definition transformer to convert Lark parse to python dict for further use.
+    Method names must match grammar rule names, thus some rules have an added -p to distinguish their name from a python
+    constant/type/default term string.
+    """
+    def action(self, content):
+        print("action cont:", content)
+
+        # action_def_dict = {'action_name': content[1].value, 'content': content[3:]}
+        action_def_dict = {'action_name': content[1].value}
+
+        for cont in content:
+            # print(type(cont))
+            if type(cont) == lark.Token:
+                # print(cont.type, cont.value)
+                pass
+            else:
+                # print("non-Token", cont)
+                if 'parameters' in cont:
+                    action_def_dict['parameters'] = cont['parameters']
+                elif 'precondition' in cont:
+                    action_def_dict['precondition'] = cont['precondition']
+                elif 'effect' in cont:
+                    action_def_dict['effect'] = cont['effect']
+
+
+        # action: lark.Tree = content[0]
+        # action_type = action.data  # main grammar rule the input was parsed as
+        # action_content = action.children  # all parsed arguments of the action 'VP'
+
+        print("action returns:", action_def_dict)
+        return action_def_dict
+        # return content
+        # pass
+
+    def parameters(self, content):
+        parameter_list = None
+        if type(content[0]) == lark.Token and content[0].type == "WS":
+            parameter_list = content[1]
+        # print("parameters:", parameter_list)
+
+        return {'parameters': parameter_list}
+
+    def precondition(self, content):
+        # print("precond cont:", content)
+        # print("precond cont:", content[1][1:])
+        return {'precondition': content[1:-1]}
+
+    def effect(self, content):
+        # print("effect cont:", content)
+        effect_dict = {'effect': content[1:-1]}
+        # print("effect returns:", effect_dict)
+        return effect_dict
+
+    def forall(self, content):
+        # print("forall cont:", content)
+        iterated_object = content[2]
+        # print("iterated object:", iterated_object)
+        forall_body = content[4:]
+        # print("forall body:", forall_body)
+
+        forall_dict = {'forall': iterated_object, 'body': forall_body}
+        # print("forall returns:", forall_dict)
+        return forall_dict
+
+    def when(self, content):
+        # print("when cont:", content)
+        when_items = list()
+        for when_item in content:
+            # ignore delimiters and whitespace:
+            if type(when_item) == lark.Token and when_item.type in ["WHENL", "WS"]:
+                pass
+            else:
+                when_items.append(when_item)
+        when_dict = {'when': when_items}
+        # print("when returns:", when_dict)
+        return when_dict
+
+    def andp(self, content):
+        # print("andp cont:", content)
+        and_items = list()
+        for and_item in content:
+            # ignore delimiters and whitespace:
+            if type(and_item) == lark.Token and and_item.type in ["ANDL", "WS"]:
+                pass
+            else:
+                and_items.append(and_item)
+        and_dict = {'and': and_items}
+        # print("andp returns:", and_dict, "\n")
+        return and_dict
+
+    def orp(self, content):
+        # print("orp cont:", content)
+        or_items = list()
+        for or_item in content:
+            # ignore delimiters and whitespace:
+            if type(or_item) == lark.Token and or_item.type in ["ORL", "WS"]:
+                pass
+            else:
+                or_items.append(or_item)
+        or_dict = {'or': or_items}
+        # print("orp returns:", or_dict, "\n")
+        return or_dict
+
+    def notp(self, content):
+        # print("notp cont:", content)
+        # (not X) always wraps only one item, hence:
+        return {'not': content[2]}
+
+    def type_list(self, content):
+        # print(content)
+        return {'type_list': content}
+
+    def type_list_item(self, content):
+        # print("type_list_item cont:", content)
+        type_list_items = list()
+        for item_element in content:
+            if 'variable' in item_element:
+                type_list_items.append(item_element)
+            elif type(item_element) == lark.Token:
+                if item_element.type == "WORD":
+                    type_list_items.append(item_element)
+                elif item_element.type == "DASH":
+                    break
+
+        if content[-1].type == "WS":
+            cat_name = content[-2].value
+        else:
+            cat_name = content[-1].value
+
+        return {'type_list_item': cat_name, 'items': type_list_items}
+
+    def equal(self, content):
+        # print("equal cont:", content)
+        # the (= X Y) can only ever take two arguments, thus directly accessing them:
+        equal_dict = {'equal': [content[2], content[4]]}
+        # print("equal returns:", equal_dict)
+        return equal_dict
+
+    def pred(self, content):
+        # print("pred content:", content)
+        if type(content[0]) == lark.Token:
+            pred_type = content[0].value
+        else:
+            pred_type = content[0]
+        # valence up to three, using None assignments to avoid downstream checks
+        pred_arg1 = None
+        pred_arg2 = None
+        pred_arg3 = None
+
+        if len(content) >= 3:
+            # print('pred arg 1:', content[2])
+            if type(content[2]) == lark.Token:
+                pred_arg1 = content[2].value
+            else:
+                pred_arg1 = content[2]
+        if len(content) >= 5:
+            if type(content[4]) == lark.Token:
+                pred_arg2 = content[4].value
+            else:
+                pred_arg2 = content[4]
+        if len(content) >= 7:
+            if type(content[6]) == lark.Token:
+                pred_arg3 = content[6].value
+            else:
+                pred_arg3 = content[6]
+
+        pred_dict = {'predicate': pred_type, 'arg1': pred_arg1, 'arg2': pred_arg2, 'arg3': pred_arg3}
+        # print(pred_dict, "\n")
+
+        return pred_dict
+
+    def var(self, content):
+        # print(content[0])
+        return {'variable': content[0].value}
+
+
+pddl_transformer = PDDLActionTransformer()
+
+action_def = pddl_transformer.transform(parsed_action)
+
+# print(action_def)
+
